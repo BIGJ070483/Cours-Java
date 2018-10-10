d9d454983f218ba2279dce40bd83e5139746ad05
BIGJ070483 <jordanguier@gmail.com>
2018-10-10 16:16:12 +0200

    adqsd
    dzsd

--- src/IncrementDecrement.java
+++ src/IncrementDecrement.java
@@ -2,6 +2,9 @@
 public class IncrementDecrement {
 
 	public static void main(String[] args) {
+		
+		//ici ont explique l'importance de l'ordre des operations
+		
 		int a = 10 ;
 		System.out.println(++a); //ici ont incremente +1 à la variable "a" avant son utilisation
 		
@@ -10,6 +13,17 @@ public class IncrementDecrement {
 									le systeme affiche d'abord le resultat de la variable puis l'incremente*/
 		System.out.println(aa); /* pour afficher le resultat de l'increment ont reaffiche la variable apres l'increment fait 
 									APRES l'affichage de la variable*/
+		
+		int part1 = 10 ;
+		int part2 = 20 ;
+		int result = part1++ + part2++ ; // ici l'increment est apres la variable donc java calcul d'abord part1+part2 puis fait l'increment
+		System.out.println(result+"-"+part1+"-"+part2); // donc ici nous avons le resultat de part1+part2= 30 puis part1++= 11 puis part2++=21
+		
+		int part3 = 10 ;
+		int part4 = 20 ;
+		int result1 = ++part3 + ++part4 ;// ici l'insrement est avant la variable donc java fait d'abord l'increment puis le calcul
+		System.out.println(result1+"-"+part3+"-"+part4);
+		
 	}
 
 }
