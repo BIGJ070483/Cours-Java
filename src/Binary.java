54c5a96e554dfc333cfe1eb79fe2f1c0337cb9aa
BIGJ070483 <jordanguier@gmail.com>
2018-10-11 15:39:15 +0200

    1

--- src/Binary.java
+++ src/Binary.java
@@ -10,19 +10,32 @@ public class Binary {
 			System.out.println(str);
 		}
 		
-		int value1 = 0b10101010 ; //ici ont exprime un chiffre en binaire avec /_\ groupement _ uniquement disponible a partir de java1.7
-		int value2 = 0b00001111 ;
+		int value1 = 0b10101010 ; //ici ont exprime un chiffre en binaire ( 170 ) avec /_\ groupement _ uniquement disponible a partir de java1.7
+		int value2 = 0b00001111 ; // ici le binaire vaut 15
 		System.out.println(value1 + value2);
 		
 		//operateur  & (AND)   | (OR)    ^ (XOR)    ~ (NOT)
-		System.out.println("NOT" + Integer.toBinaryString(~value1)); // ici ont affiche en binaire  l'inverse des bytes value1 grace a ~ qui veut dire NOT
-		System.out.println("AND" + Integer.toBinaryString(value1 & value2)); /*ici and (&) permet de comparer les couples de bytes et 
+		System.out.println("NOT " + Integer.toBinaryString(~value1)); // ici ont affiche en binaire  l'inverse des bytes value1 grace a ~ qui veut dire NOT
+		System.out.println("AND " + Integer.toBinaryString(value1 & value2)); /*ici and (&) permet de comparer les couples de bytes et 
 																		de n'afficher que si il y a bien 2 chiffres egaux 
 																		example 1 et 1  affichera 1 pour dire vrai c'est egal 
 																		et 0 et 1 pas egaux et donc il affiche 0 faux ce n'est pas egal
 																		ne pas oublier que si le resultat n'est que 0 au debut du chiffre 
 																		cela n'affichera rien */ 
-		System.out.println("OR" + Integer.toBinaryString(value1 | value2)); //ici l operateur affichera 1 (vrai)si l'un des 2 chiffres est 1
-		System.out.println("XOR" + Integer.toBinaryString(value1 ^ value2));
+		System.out.println("OR " + Integer.toBinaryString(value1 | value2)); //ici l operateur affichera 1 (vrai)si l'un des 2 chiffres est 1
+		System.out.println("XOR " + Integer.toBinaryString(value1 ^ value2)); /*ici l 'operateur va mettre 1 si il y a uniquement 1 seul 1 dans le couple de byte
+																				si il y a deux 1 il affichera 0 */
+		// << (left shift) 		>> (right shift)
+		// ici on va decale les bytes a droit ou a gauche, ce qui aura pour incidence de ou multiplier le resultat ou le deviser 
+		System.out.println("<< " + Integer.toBinaryString(value2 << 2) + " [" + value2 + " -> " + (value2 << 2) + "]" );
+		System.out.println(">> " + Integer.toBinaryString(value2 >> 2) + " [" + value2 + " -> " + (value2 >> 2) + "]" );
+		
+		// &= |= ^= >>= <<=
+		value1 <<= 2; //cela veut dire value1 = value1 << 2 ce qui veut dire qu'il multiplie value1 par 2 puis le resultat par 2
+		System.out.println( "<<=  - " + Integer.toBinaryString( value1 ) );
+	
+		int test = 3 ;
+		test <<= 4;
+		System.out.println(test);
 	}
 }
