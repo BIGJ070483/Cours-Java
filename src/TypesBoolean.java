5e49f2f5f7a4f5de721ab3a28baacfb792f0fc9a
BIGJ070483 <jordanguier@gmail.com>
2018-10-16 12:53:38 +0200

    d

---
+++ src/TypesBoolean.java
@@ -0,0 +1,9 @@
+
+public class TypesBoolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/TypesFlottants.java
+++ src/TypesFlottants.java
@@ -14,6 +14,32 @@ public class TypesFlottants {
 		double second = 1234e-3;	// notation exponentielle ( mantisse - exposant)
 		System.out.println(first + " - " + second);
 		
+		//attention piege
+		double result = 1/3; /*ici ont divise 1 (entier) par 3 (entier) forcement 1 diviser par 3 = 0
+								il faut plutot mettre 1.0/3.0 ou au moins un des chiffre avec une virgule*/
+		System.out.println(result);
 		
+		//valeurs possibles
+		double essai1 = 3.1415; //definition standard d'un double
+		double essai2 = 3/0.0;  // aura pour resultat Infinity car ont peut diviser 3 par 0 une infinité de fois
+		double essai3 = 0.0/0.0;// aura pour resultat NaN = not a number 
+		System.out.println(essai1 +"-"+essai2+"-"+essai3);
+		
+		float f2 = 3.6f;
+		float f3 = Float.NaN;
+		float f4 = Float.NEGATIVE_INFINITY;
+		System.out.println(f2 +"-"+f3+"-"+f4+"\n");
+		
+		System.out.println(Float.isFinite(f2)+" f2");
+		System.out.println(Float.isFinite(f3)+" f3");
+		System.out.println(Float.isFinite(f4)+" f4\n");
+		
+		System.out.println(Float.isInfinite(f2)+" f2");
+		System.out.println(Float.isInfinite(f3)+" f3");
+		System.out.println(Float.isInfinite(f4)+" f4\n");
+		
+		System.out.println(Float.isNaN(f2)+" f2");
+		System.out.println(Float.isNaN(f3)+" f3");
+		System.out.println(Float.isNaN(f4)+" f4\n");
 	}
 }
