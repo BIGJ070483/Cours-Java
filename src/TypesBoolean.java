15aea4118dd7a701b7503b0b5c548574fe80643c
BIGJ070483 <jordanguier@gmail.com>
2018-10-16 12:56:44 +0200

    54

--- src/TypesBoolean.java
+++ src/TypesBoolean.java
@@ -2,6 +2,8 @@
 public class TypesBoolean {
 
 	public static void main(String[] args) {
+		/*Le type booléen est un type qui définit deux états : l'état vrai et l'état faux. 
+		  Les mots clés associés sont, naturellement, boolean, true (l'état vrai) et false (l'état faux).*/
 		boolean state1 = true;
 		System.out.println(state1);
 		
