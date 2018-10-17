ea95cd6cd59f22ad14b9bb627010d4db1275683c
BIGJ070483 <jordanguier@gmail.com>
2018-10-17 17:33:00 +0200

    6754

--- src/TypeString.java
+++ src/TypeString.java
@@ -11,6 +11,17 @@ public class TypeString {
 		start += " End";				//Les classes StrinBuffer et StringBuilder peuvent etre plus efficaces ( prochain tuto)
 		System.out.println(start);
 		
+		//La classe String (son nom commence par une majuscule) propose un certain nombre de methodes
+		//Attention : une chaine de caracteres est immuable. il est impossible de changer son contenu.
+		//			  Mais on peut produire une nouvelle chaine de caracteres a partir d'une autre
+		String upper = message.toUpperCase();
+		System.out.println(upper);
+		String lower = message.toLowerCase();
+		System.out.println(lower);
+		System.out.println(message);
+		
+		String subPart = message.substring(22,29);
+		System.out.println(subPart);
 		
 	}
 
