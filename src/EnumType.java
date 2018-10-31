737964a795d026b7414a3ab65850e441a1be8e6c
BIGJ070483 <jordanguier@gmail.com>
2018-10-31 16:37:01 +0100

    4

--- src/EnumType.java
+++ src/EnumType.java
@@ -9,11 +9,27 @@ public class EnumType {
     
     public static void main(String[] args) throws Exception {
 		
-		Feu feu = Feu.VERT;
+		Feu feu = Feu.ORANGE;
 		
-		System.out.println("Que voulez-vous faire(passer, attendre) :");
+		System.out.print("Que voulez-vous faire(passer, attendre) :");
 		String action = keyboard.readLine();
 		
+		if (action.equals("passer")) {	// nb ont mets equals ici car faire == permet juste de verifier les emplacements memoires
+			if(feu != Feu.VERT) {
+				System.out.println("Vous ne pouvez pas passer, car le feu n'est pas vert");
+			} else {
+				System.out.println("Bonne route");
+			}
+		} else if (action.equals("attendre")) {
+			if(feu == Feu.VERT) {
+				System.out.println("Vous devez passer, les autres attendent derrière vous");
+			} else {
+				System.out.println("Bonne initiative");
+			}
+
+		} else {
+			System.out.println("Commande inconnue");
+		}
 	}
 
 }
