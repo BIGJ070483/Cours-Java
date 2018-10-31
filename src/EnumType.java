484abdd6150f42ebff17cf12eb8a832e212e2ac5
BIGJ070483 <jordanguier@gmail.com>
2018-10-31 15:55:13 +0100

    387475

--- src/EnumType.java
+++ src/EnumType.java
@@ -4,7 +4,11 @@ public class EnumType {
 	
 	public static void main(String[] args) {
 		
-
+		Feu feu = Feu.VERT;
+		
+		System.out.println("Que voulez-vous faire(passer, attendre) :");
+		String action = keyboard.readLine();
+		
 	}
 
 }
--- src/Feu.java
+++ src/Feu.java
@@ -1,4 +1,6 @@
 
 public enum Feu {
 
+	VERT, ORANGE, ROUGE;
+	
 }
