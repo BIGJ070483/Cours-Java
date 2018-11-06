9d133544ac41939b17f8145d2ce7e85144b7e7eb
BIGJ070483 <jordanguier@gmail.com>
2018-11-06 14:16:58 +0100

    874

--- src/TestIf.java
+++ src/TestIf.java
@@ -8,7 +8,7 @@ public class TestIf {
 		if (value > 5) {
 			System.out.println(value + " - BIG");
 			System.out.println("SUITE");
-		} else 	{		/* que ce soit pour if ou else ou n'importe quel element
+		} else {		/* que ce soit pour if ou else ou n'importe quel element
 		si ont ne mets pas { cela va executé la 1ere instructions ( ici System.out.println(value + " - SMALL"); )
 		l'instruction suivante sera automatiquement executé quel que soit la ligne precedente
 		il faut donc bien reflechir a l'indentation et surtout a la declaration de nos blocs*/
